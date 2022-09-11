539726d8b9e017f4920aaf993810e038e5e8cbe1
IvanKristiawan <101783828+IvanKristiawan@users.noreply.github.com>
2022-09-11 11:12:06 +0700

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,136 @@
+const logotext = "IVAN";
+const meta = {
+  title: "Ivan Kristiawan",
+  description:
+    "I’m Ivan Kristiawan _ Full stack developer,currently working in Indonesia"
+};
+
+const introdata = {
+  title: "I’m Ivan Kristiawan",
+  animated: {
+    first: "I love code",
+    second: "I code business websites",
+    third: "I develop mobile apps"
+  },
+  description:
+    "Hi, my name is Ivan Kristiawan. I'm a programmer from Indonesia. I combine my abilities of coding and business. I create website and mobile apps for business needs.",
+  your_img_url:
+    "https://res.cloudinary.com/dbtag5lau/image/upload/v1662865283/Portfolio%20Ivan/IvanImg.jpg"
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "I'm programmer from Indonesia. I have an extensive knowledge about programming and business. I really like to make system to better operate businesses. I mainly work as Fullstack Website Programmer and Mobile Apps Programmer."
+};
+const worktimeline = [
+  {
+    jobtitle: "Fullstack Website Programmer",
+    where: "IKP",
+    date: "2022"
+  },
+  {
+    jobtitle: "Fullstack Website Programmer",
+    where: "Freelance",
+    date: "2020"
+  },
+  {
+    jobtitle: "UI/UX Designer",
+    where: "Freelance",
+    date: "2019"
+  }
+];
+
+const skills = [
+  {
+    name: "Javascript",
+    value: 95
+  },
+  {
+    name: "React",
+    value: 90
+  },
+  {
+    name: "NodeJs",
+    value: 90
+  },
+  {
+    name: "React Native",
+    value: 70
+  },
+  {
+    name: "Python",
+    value: 60
+  }
+];
+
+const services = [
+  {
+    title: "Website Programming",
+    description:
+      "I create Website that can be customized to meet your business needs. All of my service can be paid monthly as a part of maintain Product fees."
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "I create Mobile Apps that can be customized to meet your business needs. All of my service can be paid monthly as a part of maintain Product fees."
+  }
+];
+
+const dataportfolio = [
+  {
+    img:
+      "https://res.cloudinary.com/dbtag5lau/image/upload/v1662865878/Portfolio%20Ivan/POSUi.png",
+    desctiption: "Website for Point Of Sale and Bookeeping.",
+    link: "https://posbes.itkitapro.com/"
+  },
+  {
+    img:
+      "https://res.cloudinary.com/dbtag5lau/image/upload/v1662865857/Portfolio%20Ivan/AkuntiUi.png",
+    desctiption: "Website for Bookeeping Company.",
+    link: "https://akunti.itkitapro.com/"
+  },
+  {
+    img:
+      "https://res.cloudinary.com/dbtag5lau/image/upload/v1662865868/Portfolio%20Ivan/PetCareUi.png",
+    desctiption: "Website for animals health service.",
+    link: "https://petcare-indonesia.herokuapp.com/"
+  },
+  {
+    img:
+      "https://res.cloudinary.com/dbtag5lau/image/upload/v1662866687/Portfolio%20Ivan/IKPProfile.png",
+    desctiption: "Website to display Company Profile.",
+    link: "https://company.itkitapro.com/"
+  }
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "ivanteens@gmail.com",
+  YOUR_FONE: "+62895421397226",
+  YOUR_IG: "@ivankrist02",
+  description:
+    "Feel free to reaching out for Me. I'm looking forward to work with you!",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id"
+};
+
+const socialprofils = {
+  github: "https://github.com/IvanKristiawan",
+  whatsapp: "https://wa.me/+62895421397226",
+  instagram: "https://www.instagram.com/ivankrist02/"
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext
+};
